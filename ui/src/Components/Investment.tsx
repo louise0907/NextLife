c5b85ddac00f14b990aaecabcc277a0acbbf91e9
unknown <fourtune45@gmail.com>
2024-05-13 14:14:47 +0800

    STYLE: add sorting for table

--- ui/src/Components/Investment.tsx
+++ ui/src/Components/Investment.tsx
@@ -1,4 +1,6 @@
 import { useState, useEffect, ReactNode } from 'react'
+
+//Import APIS
 import NetworthFinder from '../Apis/NetworthFinder'
 
 //import icon
@@ -13,6 +15,8 @@ interface Data {
   value: number
   base_value: number
   investment: boolean
+  profit_myr: number
+  profit_percentage: number
   date: Date
 }
 
@@ -23,6 +27,32 @@ const Investment = () => {
   const [totalProfit, setTotalProfit] = useState(0)
   let value = 0
   let capital = 0
+
+  const [sortConfig, setSortConfig] = useState<{
+    key: keyof Data
+    direction: string
+  }>({
+    key: 'id',
+    direction: 'desc',
+  })
+
+  const handleSort = (key: keyof Data) => {
+    let direction = 'asc'
+    if (sortConfig.key === key && sortConfig.direction === 'asc') {
+      direction = 'desc'
+    }
+    setDatas(
+      [...datas].sort((a, b) => {
+        // Ensure that TypeScript recognizes the key as a valid property of Data
+        const aValue = a[key]
+        const bValue = b[key]
+        if (aValue < bValue) return direction === 'asc' ? -1 : 1
+        if (aValue > bValue) return direction === 'asc' ? 1 : -1
+        return 0
+      })
+    )
+    setSortConfig({ key, direction })
+  }
 
   const calc = () => {
     for (let i = 0; i < datas.length; i++) {
@@ -41,22 +71,40 @@ const Investment = () => {
         const response = await NetworthFinder.get('/')
         if (response.data.data.networth.length !== 0) {
           // Filter and update datas state with only invest true data
+          // const filteredData: Data[] = response.data.data.networth.filter(
+          //   (data: Data) => data.investment
+          // )
+          // setDatas(filteredData)
+
           const filteredData: Data[] = response.data.data.networth.filter(
             (data: Data) => data.investment
           )
-          setDatas(filteredData)
+          console.log('Filtered Data:', filteredData) // Log the filtered data
+          // Initialize profit_myr and profit_percentage properties for each data item
+          const updatedData: Data[] = filteredData.map((data: Data) => ({
+            ...data,
+            profit_myr: data.value - data.base_value, // Calculate initial profit_myr
+            profit_percentage: parseFloat(
+              (
+                ((data.value - data.base_value) / data.base_value) *
+                100
+              ).toFixed(2)
+            ), // Calculate initial profit_percentage
+          }))
+          console.log('Updated Data:', updatedData) // Log the updated data
+          setDatas(updatedData)
         }
       } catch (error) {
         console.log(error)
       }
     }
 
-    // Call fetchData only once after initial render
     fetchData()
   }, [])
 
   useEffect(() => {
     calc()
+    // handleSort('profit_percentage')
   }, [datas])
 
   return (
@@ -127,28 +175,43 @@ const Investment = () => {
             <tr>
               <th
                 scope='col'
-                className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
-                Investment
+                className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+                onClick={() => handleSort('name')}>
+                Investment{' '}
+                {sortConfig.key === 'name' &&
+                  (sortConfig.direction === 'asc' ? '▲' : '▼')}
               </th>
               <th
                 scope='col'
-                className='px-6 py-3 text-left text-xs text-center align-middle font-medium text-gray-500 uppercase tracking-wider'>
-                Total (MYR)
+                className='px-6 py-3 text-left text-xs text-center align-middle font-medium text-gray-500 uppercase tracking-wider'
+                onClick={() => handleSort('value')}>
+                Total (MYR){' '}
+                {sortConfig.key === 'id' &&
+                  (sortConfig.direction === 'asc' ? '▲' : '▼')}
               </th>
               <th
                 scope='col'
-                className='px-6 py-3 text-left text-xs text-center align-middle font-medium text-gray-500 uppercase tracking-wider'>
-                Capital (MYR)
+                className='px-6 py-3 text-left text-xs text-center align-middle font-medium text-gray-500 uppercase tracking-wider'
+                onClick={() => handleSort('base_value')}>
+                Capital (MYR){' '}
+                {sortConfig.key === 'id' &&
+                  (sortConfig.direction === 'asc' ? '▲' : '▼')}
               </th>
               <th
                 scope='col'
-                className='px-6 py-3 text-left text-xs text-center align-middle font-medium text-gray-500 uppercase tracking-wider'>
-                Profit (MYR)
+                className='px-6 py-3 text-left text-xs text-center align-middle font-medium text-gray-500 uppercase tracking-wider'
+                onClick={() => handleSort('profit_myr')}>
+                Profit (MYR){' '}
+                {sortConfig.key === 'id' &&
+                  (sortConfig.direction === 'asc' ? '▲' : '▼')}
               </th>
               <th
                 scope='col'
-                className='px-6 py-3 text-left text-xs text-center align-middle font-medium text-gray-500 uppercase tracking-wider'>
-                Profit (%)
+                className='px-6 py-3 text-left text-xs text-center align-middle font-medium text-gray-500 uppercase tracking-wider'
+                onClick={() => handleSort('profit_percentage')}>
+                Profit (%){' '}
+                {sortConfig.key === 'id' &&
+                  (sortConfig.direction === 'asc' ? '▲' : '▼')}
               </th>
             </tr>
           </thead>
@@ -164,13 +227,10 @@ const Investment = () => {
                     {data.base_value}
                   </td>
                   <td className='px-6 py-4 text-center align-middle whitespace-nowrap'>
-                    {data.value - data.base_value}
+                    {data.profit_myr}
                   </td>
                   <td className='px-6 py-4 text-center align-middle whitespace-nowrap'>
-                    {(
-                      ((data.value - data.base_value) / data.base_value) *
-                      100
-                    ).toFixed(2)}
+                    {data.profit_percentage}
                   </td>
                 </tr>
               ))}
